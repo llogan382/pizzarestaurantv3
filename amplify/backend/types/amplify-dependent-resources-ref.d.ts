41d29be67853e0426f6ce505e9471a19edb44b25
Luke <llogan382@gmail.com>
2021-12-17 06:04:01 -0500

    adds files

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,9 @@
+export type AmplifyDependentResourcesAttributes = {
+    "api": {
+        "pizzarestaurantv3": {
+            "GraphQLAPIKeyOutput": "string",
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        }
+    }
+}
